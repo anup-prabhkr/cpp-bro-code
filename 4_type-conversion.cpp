57b400d80263f81af78069269a0d84c48f570ef3
anup-prabhkr <anupprabhakar01+code@gmail.com>
2025-08-19 14:53:21 +0530

    modified tyep-conversion

--- 4_type-conversion.cpp
+++ 4_type-conversion.cpp
@@ -3,12 +3,26 @@
 int main(){
 
 int x = 3.14;
+
 double y = (int) 6.93;// explicit type conversion (int to float)
 
+char z = 65; // implicit
 
 std::cout << x << '\n'; // Implicit type conversion (float to int)
 
 std::cout << y << '\n';
+
+std::cout << z << '\n';
+
+std::cout << (char) 97 << '\n'; //explicit
+
+//we can use type converion in int division
+
+int a = 7;
+int b = 13;
+double score = (double)a/b * 100; // expicit
+
+std::cout << score << "%" << '\n';
 
 return 0;
 }
